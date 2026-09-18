f39ecc6cc86f5ccf27365bcf030775acb075d5fe
fireheart071 <edmundamonoo@gmail.com>
2026-09-18 14:27:45 +0000

    Add vercel.json Astro configuration and standard Go entrypoints

---
+++ server/cmd/server/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/fireheart071/config"
+	"github.com/fireheart071/routes"
+	"github.com/fireheart071/seeds"
+	"github.com/gofiber/fiber/v3"
+	"github.com/gofiber/fiber/v3/middleware/cors"
+	"github.com/gofiber/fiber/v3/middleware/logger"
+	"github.com/gofiber/fiber/v3/middleware/recover"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	if err := godotenv.Load(); err != nil {
+		_ = godotenv.Load("../../.env")
+	}
+
+	db := config.InitDB()
+	seeds.SeedDatabase(db)
+
+	app := fiber.New(fiber.Config{
+		AppName: "Rabbi Azanduna Ltd Luxury API",
+	})
+
+	app.Use(logger.New())
+	app.Use(recover.New())
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: []string{"*"},
+		AllowHeaders: []string{"Origin", "Content-Type", "Accept", "Authorization"},
+		AllowMethods: []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+	}))
+
+	routes.SetupRoutes(app, db)
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8085"
+	}
+
+	log.Printf("[Server] Starting Rabbi Azanduna Ltd Luxury API on port :%s ...", port)
+	log.Fatal(app.Listen(":" + port))
+}
