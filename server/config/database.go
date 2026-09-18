a68cbee12b70ab846078c2731d501dbb1d552749
fireheart071 <edmundamonoo@gmail.com>
2026-09-18 14:05:09 +0000

    Configure direct MySQL database connection with automated schema creation

--- server/config/database.go
+++ server/config/database.go
@@ -1,11 +1,13 @@
 package config
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"os"
 
 	"github.com/fireheart071/models"
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/joho/godotenv"
 	"gorm.io/driver/mysql"
 	"gorm.io/driver/sqlite"
@@ -15,24 +17,55 @@ import (
 
 var DB *gorm.DB
 
+// ensureMySQLDatabase tries to create the database on the MySQL instance if it doesn't already exist.
+func ensureMySQLDatabase(user, pass, host, port, dbName string) error {
+	rootDSN := fmt.Sprintf("%s:%s@tcp(%s:%s)/?charset=utf8mb4&parseTime=True&loc=Local",
+		user, pass, host, port)
+	sqlDB, err := sql.Open("mysql", rootDSN)
+	if err != nil {
+		return err
+	}
+	defer sqlDB.Close()
+
+	if err := sqlDB.Ping(); err != nil {
+		return err
+	}
+
+	query := fmt.Sprintf("CREATE DATABASE IF NOT EXISTS `%s` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;", dbName)
+	_, err = sqlDB.Exec(query)
+	return err
+}
+
 func InitDB() *gorm.DB {
 	_ = godotenv.Load()
 
+	dbType := os.Getenv("DB_TYPE")
+	if dbType == "" {
+		dbType = "mysql"
+	}
+
+	dbUser := os.Getenv("DB_USER")
+	if dbUser == "" {
+		dbUser = "root"
+	}
+	dbPass := os.Getenv("DB_PASS")
+	dbHost := os.Getenv("DB_HOST")
+	if dbHost == "" {
+		dbHost = "127.0.0.1"
+	}
+	dbPort := os.Getenv("DB_PORT")
+	if dbPort == "" {
+		dbPort = "3306"
+	}
+	dbName := os.Getenv("DB_NAME")
+	if dbName == "" {
+		dbName = "rabbi_azanduna_db"
+	}
+
 	dsn := os.Getenv("MYSQL_DSN")
 	if dsn == "" {
-		dbUser := os.Getenv("DB_USER")
-		dbPass := os.Getenv("DB_PASS")
-		dbHost := os.Getenv("DB_HOST")
-		dbPort := os.Getenv("DB_PORT")
-		dbName := os.Getenv("DB_NAME")
-
-		if dbHost != "" && dbName != "" {
-			if dbPort == "" {
-				dbPort = "3306"
-			}
-			dsn = fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
-				dbUser, dbPass, dbHost, dbPort, dbName)
-		}
+		dsn = fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+			dbUser, dbPass, dbHost, dbPort, dbName)
 	}
 
 	var db *gorm.DB
@@ -42,17 +75,24 @@ func InitDB() *gorm.DB {
 		Logger: logger.Default.LogMode(logger.Info),
 	}
 
-	if dsn != "" {
-		log.Printf("[DB] Connecting to MySQL database...")
+	if dbType == "mysql" {
+		log.Printf("[DB] Connecting to MySQL database '%s' on %s:%s (User: %s)...", dbName, dbHost, dbPort, dbUser)
+
+		// Ensure the database exists on the MySQL instance
+		if createErr := ensureMySQLDatabase(dbUser, dbPass, dbHost, dbPort, dbName); createErr != nil {
+			log.Printf("[DB Notice] Automated database creation check: %v", createErr)
+		}
+
 		db, err = gorm.Open(mysql.Open(dsn), gormConfig)
 		if err != nil {
-			log.Printf("[DB] Failed to connect to MySQL (%v). Falling back to SQLite for local development.", err)
+			log.Printf("[DB Warning] Failed to connect to MySQL database: %v", err)
+			log.Printf("[DB Notice] Falling back to local SQLite database ('oud_attar.db') so the application remains operable.")
 			db, err = gorm.Open(sqlite.Open("oud_attar.db"), gormConfig)
 		} else {
-			log.Printf("[DB] Successfully connected to MySQL database.")
+			log.Printf("[DB] Successfully connected to MySQL database: %s", dbName)
 		}
 	} else {
-		log.Printf("[DB] No MySQL DSN provided. Using SQLite database ('oud_attar.db') for local development.")
+		log.Printf("[DB] DB_TYPE is set to sqlite. Using SQLite database ('oud_attar.db').")
 		db, err = gorm.Open(sqlite.Open("oud_attar.db"), gormConfig)
 	}
 
--- server/go.mod
+++ server/go.mod
@@ -17,6 +17,10 @@ require (
 	github.com/gofiber/schema v1.8.3 // indirect
 	github.com/gofiber/utils/v2 v2.4.1 // indirect
 	github.com/google/uuid v1.6.0 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
+	github.com/jackc/pgx/v5 v5.10.0 // indirect
+	github.com/jackc/puddle/v2 v2.2.2 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/klauspost/compress v1.19.2 // indirect
@@ -29,6 +33,8 @@ require (
 	github.com/valyala/fasthttp v1.73.0 // indirect
 	golang.org/x/crypto v0.54.0 // indirect
 	golang.org/x/net v0.57.0 // indirect
+	golang.org/x/sync v0.22.0 // indirect
 	golang.org/x/sys v0.47.0 // indirect
 	golang.org/x/text v0.40.0 // indirect
+	gorm.io/driver/postgres v1.6.3 // indirect
 )
